70997c8d6cf36fe933cf745d1d41f75b26087986
Wayne Collier <ke4tri@gmail.com>
2018-10-16 18:44:20 -0500

    printing fish double

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,42 @@
-// Filter fish that are "on sale"
 
-
-
-// Add fish to "Basket"
+const writeFishes = (arrayOfFishes) => {
+    let domString = '';
+    arrayOfFishes.forEach((fish) =>{
+      domString += `
+      <div class="${fish.onSale ? 'on-sale' : '' } fish card col-md-6 col-md-offset-3">
+      <div class="thumbnail">
+                <img src="${fish.imageSoure}"
+        " alt="" width="40%">
+                <div class="caption">
+                    <h3 id="thumbnail-label">Fish 4</h3>
+                    <p>$
+                        <span class="price">19</span>
+                    </p>
+                </div>
+                <div class="caption card-footer">
+                    <button class="add btn btn-danger">Add To Basket</button>
+                </div>
+            </div>
+        </div>
+        ` 
+    })
+        $("#available").append(domString);
+        $(domString).append("#available");
+}
 
 // Load Fish
 $.get('../db/fishes.json')
     .done((data) => {
         console.log(data);
+        writeFishes(data.fishes);
     })
     .fail((error) => {
         console.error(error);
     });    // only simicolon you have should be here
 
 
+
         
+
+
+
